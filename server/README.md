ffccdf3cfc7df71de336d7b2e19147d1fae3007b
Russ Garrett <russ@garrett.co.uk>
2021-10-08 10:46:47 +0100

    Initial tile server code

---
+++ server/README.md
@@ -0,0 +1,13 @@
+# Tile Server
+
+This is a simple tile server which serves the address data as tiled GeoJSON using the same ["Slippy map" tiling convention](https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames) as OSM map tiles.
+
+It requires the PostGIS database URL to be provided as an environment variable, e.g.:
+
+	DATABASE_URL="postgresql://user:password@host/osm_addresses"
+
+An example URL is:
+
+	http://127.0.0.1:8000/addresses/17/65492/43567.json
+
+(Only zoom levels 16-21 are currently supported)
---
+++ server/geojson_tile_server/main.py
@@ -0,0 +1,70 @@
+import databases
+import mercantile
+from starlette.applications import Starlette
+from starlette.config import Config
+from starlette.exceptions import HTTPException
+from starlette.responses import Response, FileResponse
+from starlette.routing import Route, Mount
+from starlette.staticfiles import StaticFiles
+
+
+config = Config(".env")
+DATABASE_URL = config("DATABASE_URL")
+
+
+database = databases.Database(DATABASE_URL)
+
+
+async def serve(request):
+    x, y, z = (
+        request.path_params["x"],
+        request.path_params["y"],
+        request.path_params["z"],
+    )
+
+    # TODO: make zoom range configurable
+    if z < 16 or z > 21:
+        raise HTTPException(400, "Z coordinate out of range")
+
+    if x < 0 or x > 2 ** z:
+        raise HTTPException(400, "X coordinate out of range")
+
+    if y < 0 or y > 2 ** z:
+        raise HTTPException(400, "Y coordinate out of range")
+
+    bounds = mercantile.bounds(x, y, z)
+
+    row = await database.fetch_one(
+        query="""
+        WITH row AS (
+            SELECT ST_Transform(ST_PointOnSurface(geometry), 4326) AS geometry, inspireid
+            FROM split_buildings
+            WHERE ST_Contains(
+                ST_Transform(ST_MakeEnvelope(:minx, :miny, :maxx, :maxy, 4326), 27700),
+                geometry)
+        )
+        SELECT json_build_object('type', 'FeatureCollection', 'features', json_agg(ST_AsGeoJSON(row)::json))
+        FROM row""",
+        values={
+            "minx": bounds.west,
+            "miny": bounds.south,
+            "maxx": bounds.east,
+            "maxy": bounds.north,
+        },
+    )
+    return Response(row[0], media_type="application/json")
+
+
+async def index(request):
+    return FileResponse('index.html')
+
+
+routes = [
+    #  Route('/', endpoint=index, name="index", methods=["GET"]),
+    #  Mount('/static', app=StaticFiles(directory='static'), name="static"),
+    Route("/addresses/{z:int}/{x:int}/{y:int}.json", endpoint=serve, methods=["GET"]),
+]
+
+app = Starlette(
+    routes=routes, on_startup=[database.connect], on_shutdown=[database.disconnect]
+)
