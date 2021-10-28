3cc1ba493638440cc4ddca6b3f0a674d7029fa90
Russ Garrett <russ@garrett.co.uk>
2021-10-28 18:33:21 +0100

    Add additional layers

--- server/README.md
+++ server/README.md
@@ -4,10 +4,14 @@ This is a simple tile server which serves the address data as tiled GeoJSON usin
 
 It requires the PostGIS database URL to be provided as an environment variable, e.g.:
 
-	DATABASE_URL="postgresql://user:password@host/osm_addresses"
+    DATABASE_URL="postgresql://user:password@host/osm_addresses"
+
+To run a development instance with reloading:
+
+    uvicorn --reload geojson_tile_server.main:app
 
 An example URL is:
 
-	http://127.0.0.1:8000/addresses/17/65492/43567.json
+    http://127.0.0.1:8000/addresses/17/65492/43567.json
 
 (Only zoom levels 16-21 are currently supported)
--- server/geojson_tile_server/main.py
+++ server/geojson_tile_server/main.py
@@ -16,8 +16,31 @@ DATABASE_URL = config("DATABASE_URL")
 
 database = databases.Database(DATABASE_URL)
 
+LAYERS = {
+    "addresses": """
+        SELECT ST_Transform(ST_PointOnSurface(geometry), 4326) AS geometry, inspireid
+            FROM split_buildings
+            WHERE ST_Intersects(ST_Transform({bbox}, 27700), geometry)
+    """,
+    "inspire": """
+        SELECT ST_Transform(wkb_geometry, 4326) AS geometry, inspireid
+            FROM inspire
+            WHERE ST_Intersects(ST_Transform({bbox}, 27700), wkb_geometry)
+    """,
+    "split_buildings": """
+        SELECT ST_Transform(geometry, 4326) AS geometry, inspireid
+            FROM split_buildings
+            WHERE ST_Intersects(ST_Transform({bbox}, 27700), geometry)
+    """,
+}
+
 
 async def serve(request):
+    layer = request.path_params["layer"]
+
+    if layer not in LAYERS:
+        raise HTTPException(404)
+
     x, y, z = (
         request.path_params["x"],
         request.path_params["y"],
@@ -25,7 +48,7 @@ async def serve(request):
     )
 
     # TODO: make zoom range configurable
-    if z < 16 or z > 21:
+    if z < 16 or z >= 21:
         raise HTTPException(400, "Z coordinate out of range")
 
     if x < 0 or x > 2 ** z:
@@ -36,41 +59,37 @@ async def serve(request):
 
     bounds = mercantile.bounds(x, y, z)
 
+    sql = LAYERS[layer].format(
+        bbox=f"ST_MakeEnvelope({bounds.west}, {bounds.south}, {bounds.east}, {bounds.north}, 4326)"
+    )
     row = await database.fetch_one(
-        query="""
-        WITH row AS (
-            SELECT ST_Transform(ST_PointOnSurface(geometry), 4326) AS geometry, inspireid
-            FROM split_buildings
-            WHERE ST_Contains(
-                ST_Transform(ST_MakeEnvelope(:minx, :miny, :maxx, :maxy, 4326), 27700),
-                geometry)
-        )
+        query=f"""
+        WITH row AS ({sql})
         SELECT json_build_object('type', 'FeatureCollection', 'features', json_agg(ST_AsGeoJSON(row)::json))
-        FROM row""",
-        values={
-            "minx": bounds.west,
-            "miny": bounds.south,
-            "maxx": bounds.east,
-            "maxy": bounds.north,
-        },
+        FROM row"""
     )
     return Response(row[0], media_type="application/json")
 
 
 async def index(request):
-    return FileResponse('index.html')
+    return FileResponse("index.html")
 
 
 routes = [
     #  Route('/', endpoint=index, name="index", methods=["GET"]),
     #  Mount('/static', app=StaticFiles(directory='static'), name="static"),
-    Route("/addresses/{z:int}/{x:int}/{y:int}.json", endpoint=serve, methods=["GET"]),
+    Route(
+        "/{layer:str}/{z:int}/{x:int}/{y:int}.json",
+        endpoint=serve,
+        methods=["GET"],
+    ),
 ]
 
-middleware = [
-    Middleware(CORSMiddleware, allow_origins=['*'])
-]
+middleware = [Middleware(CORSMiddleware, allow_origins=["*"])]
 
 app = Starlette(
-    routes=routes, middleware=middleware, on_startup=[database.connect], on_shutdown=[database.disconnect]
+    routes=routes,
+    middleware=middleware,
+    on_startup=[database.connect],
+    on_shutdown=[database.disconnect],
 )
